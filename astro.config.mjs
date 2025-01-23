9fd31050a8732b09d8188f5c4ecde08704332af6
0xx32 <dimas1523@mail.ru>
2025-01-23 17:12:19 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
+import node from "@astrojs/node";
+import vue from "@astrojs/vue";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [vue(), tailwind(), db()],
+
+	adapter: node({
+		mode: "standalone",
+	}),
+});
